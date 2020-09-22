e550631bc22e4cb8446f8f9874d897bb0ffc9915
Abinash Kushwaha <abinash3027@users.noreply.github.com>
2020-09-22 13:20:29 +0545

    Initial commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python3 -m OstaraBot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE Ostara Bot
+:: Enables virtual env mode and then starts Ostara
+env\scripts\activate.bat && py -m OstaraBot
