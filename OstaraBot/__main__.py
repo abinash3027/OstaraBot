3d12cdc4ed9424ba02b8a91dded6b1eb4ff7cbaa
Abinash Kushwaha <abinash3027@users.noreply.github.com>
2020-12-10 12:56:10 +0545

    Update __main__.py

--- OstaraBot/__main__.py
+++ OstaraBot/__main__.py
@@ -52,15 +52,13 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """
 Hi {}, my name is {}! 
-I am an Anime themed group management bot.
-Build by weebs for weebs, I specialize in managing anime and similar themed groups.
+I am a modular group management bot.
+
 You can find my list of available commands with /help.
 """
 
-HELP_STRINGS = """
-Hey there! My name is *{}*.
-I'm a Hero For Fun and help admins manage their groups with One Punch! Have a look at the following for an idea of some of \
-the things I can help you with.
+HELP_STRINGS = """Hey there! My name is *{}*. 
+I'm a modular group management bot and help admins manage their groups. Have a look at the following for an idea of some of the things I can help you with. 
 
 *Main* commands available:
  • /help: PM's you this message.
